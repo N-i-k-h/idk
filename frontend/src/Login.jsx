429e7f1d412c3a49333ee2f3ba2a851880882854
N-i-k-h <nikhilkashyap4217@gmail.com>
2025-03-09 20:21:46 +0530

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("https://facultyside.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
