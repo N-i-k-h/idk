7d880c1ae975c474fff82c420b5f1ba93d259fa1
N-i-k-h <nikhilkashyap4217@gmail.com>
2025-03-09 20:23:02 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./register";
-import Login from "./login";
+import Register from "./Register";
+import Login from "./Login";
 import App from "./App";
 import "./index.css";
 
