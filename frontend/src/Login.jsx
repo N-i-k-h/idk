4084231aa698c012c071de41e8ef9a4cd16a34c7
N-i-k-h <nikhilkashyap4217@gmail.com>
2025-04-17 22:19:58 +0530

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("https://idk-4-50nz.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
