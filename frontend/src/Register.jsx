96028334787d81d896996fa4e9fee8bc34fa24ea
N-i-k-h <nikhilkashyap4217@gmail.com>
2025-04-17 22:45:12 +0530

    Update Register.jsx

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -27,7 +27,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch("https://idk-4-50nz.onrender.com/register", {
+      const response = await fetch("http://localhost:5000/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
