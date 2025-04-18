6fa15a03e9b347a1c70eeca4e3e7e15700a209fb
N-i-k-h <nikhilkashyap4217@gmail.com>
2025-04-18 10:07:37 +0530

    Update AdminLogin.jsx

--- frontend/src/AdminLogin.jsx
+++ frontend/src/AdminLogin.jsx
@@ -13,7 +13,7 @@ const AdminLogin = () => {
     setError("");
 
     try {
-      const response = await fetch("https://back-w3b4.onrender.com/admin/login", {
+      const response = await fetch("https://idb.onrender.com/admin/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
