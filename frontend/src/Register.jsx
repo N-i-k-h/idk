68dbf4c2def9df59d1d5abcf1b62a2e4c64ab0fa
N-i-k-h <nikhilkashyap4217@gmail.com>
2025-04-23 13:45:12 +0530

    Update Register.jsx

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -30,7 +30,7 @@ const Register = () => {
 
     setLoading(true);
     try {
-      const response = await fetch("https://idb.onrender.com/register", {
+      const response = await fetch("https://ind-54pe.onrender.com/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
