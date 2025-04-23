1d83c52b0e0f76bd300a155fbfbb7e09f56d7f3c
N-i-k-h <nikhilkashyap4217@gmail.com>
2025-04-23 15:14:52 +0530

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     setError("");
 
     try {
-      const response = await fetch("https://idkba.onrender.com/login", {
+      const response = await fetch("https://idk-5ba.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
